abc00fc53d1f70b205d00b70d29725a514a5bf9d
Nicolay Mitropolsky <nicolay.mitropolsky@jetbrains.com>
2019-11-05 12:27:22 +0300

    Vue + WebSockets

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
@@ -13,6 +14,7 @@ import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 import javax.servlet.http.HttpServletRequest;
 
 @SpringBootApplication
+@EnableWebSocket
 public class DemoApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -1,10 +1,19 @@
 package com.example.demo;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -13,6 +22,41 @@ import java.util.List;
 
 @RestController
 public class MyController {
+
+    List<WebSocketSession> webSocketSessions = Collections.synchronizedList(new ArrayList<>());
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Bean
+    WebSocketConfigurer webSocketConfigurer() {
+        return new WebSocketConfigurer() {
+            @Override
+            public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+                registry.addHandler(new TextWebSocketHandler() {
+                    @Override
+                    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+                        webSocketSessions.add(session);
+                    }
+
+                    @Override
+                    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+                        webSocketSessions.remove(session);
+                    }
+
+                    @Override
+                    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+                        String payload = message.getPayload();
+                        Product product = objectMapper.readValue(payload, Product.class);
+                        products.add(product);
+                        for (WebSocketSession webSocketSession : webSocketSessions) {
+                            webSocketSession.sendMessage(new TextMessage(objectMapper.writeValueAsString(products)));
+                        }
+                    }
+                }, "/ws");
+            }
+        };
+    }
 
     List<SseEmitter> sseEmitters = Collections.synchronizedList(new ArrayList<>());
 
